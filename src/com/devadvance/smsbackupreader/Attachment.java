f18810f59c1a0efecc3226828dcb974f36c75fa3
mrussojr <mrussojr@users.noreply.github.com>
2019-03-15 14:31:49 -0400

    MMS and Attachment Support
    Added support for MMS messages from the back-up file, as well as support for attachments.  Currently supports images only.

---
+++ src/com/devadvance/smsbackupreader/Attachment.java
@@ -0,0 +1,56 @@
+package com.devadvance.smsbackupreader;
+
+public class Attachment {
+	private String mimeType;
+	private String base64Data;
+	private String fileName;
+	
+	public Attachment(String _mimeType, String _base64Data, String _fileName){
+		mimeType = _mimeType;
+		base64Data = _base64Data;
+		fileName = _fileName;
+	}
+	
+	public String getMimeType(){
+		return mimeType;
+	}
+	
+	public void setMimeType(String input){
+		mimeType = input;
+	}
+	
+	public String getFileName(){
+		return fileName;
+	}
+	
+	public void setFileName(String input){
+		fileName = input;
+	}
+	
+	public String getBase64Data(){
+		return base64Data;
+	}
+	
+	public void setBase64Data(String input){
+		base64Data = input;
+	}
+
+	public String toHtml(){
+		String tempString;
+		
+		tempString = "<a href=\"" + base64Data + "\">" + fileName + "</a>";
+		
+		return tempString;
+	}
+
+	@Override
+	public String toString(){
+		String tempString;
+		
+		tempString = "\n";
+		tempString += "\tAttachment Name: " + fileName + "\n";
+		tempString += "\tAttachment Type: " + mimeType;
+		
+		return tempString;
+	}
+}
--- src/com/devadvance/smsbackupreader/Message.java
+++ src/com/devadvance/smsbackupreader/Message.java
@@ -2,6 +2,7 @@ package com.devadvance.smsbackupreader;
 
 import java.math.BigInteger;
 import java.util.Date;
+import java.util.List;
 
 public class Message implements Comparable<Message> {
 	private String messageText;
@@ -9,6 +10,7 @@ public class Message implements Comparable<Message> {
 	private BigInteger messageDate;
 	private BigInteger messageAddress;
 	private Date messageDateFormat;
+	private List<Attachment> messageAttachments;
 
 	public Message() {
 		messageType = -1;
@@ -38,6 +40,29 @@ public class Message implements Comparable<Message> {
 		messageDateFormat = new Date(messageDate.longValue());
 	}
 
+	/**
+	 * Constructor to create a message with specified parameters.
+	 * @param address address of the message.
+	 * @param date date of the message.
+	 * @param body text of the message;
+	 * @param type Type of message, 1 is received, 0 is sent.
+	 * @param offset Offset if it is a received message.
+	 * @param attachments Attachments from MMS message.
+	 */
+	public Message(BigInteger address,BigInteger date,String body,int type, BigInteger offset, List<Attachment> attachments) {
+		messageType = type;
+
+		messageDate = date;
+		// If it is a received message, add the offset
+		if (messageType == 1)
+			messageDate = messageDate.add(offset);
+		messageText = body;
+		messageAddress = address;
+		messageDateFormat = new Date(messageDate.longValue());
+		
+		messageAttachments = attachments;
+	}
+	
 	@Override
 	public int compareTo(Message msg2) { 
 
@@ -77,6 +102,31 @@ public class Message implements Comparable<Message> {
 		return messageAddress;
 	}
 
+	public String toHtml() {
+		String tempString;
+		String fromTo;
+		
+		tempString = "<div class=\"message-";
+		
+		if (messageType == 2)
+			fromTo = "to\">";
+		else
+			fromTo = "from\">";
+		//tempString += messageDateFormat.toString() + ":  ";
+		tempString += fromTo + messageText;
+		
+		if(messageAttachments != null && messageAttachments.size() > 0){
+			for(int i = 0; i < messageAttachments.size(); i++){
+				tempString += messageAttachments.get(i).toHtml();
+			}
+		}
+		
+		tempString += "<div class=\"date-" + fromTo + messageDateFormat.toString() + "</div>";
+		tempString += "</div>";
+		
+		return tempString;
+	}
+	
 	@Override
 	public String toString() {
 		String tempString;
@@ -86,6 +136,13 @@ public class Message implements Comparable<Message> {
 			tempString = "Received: ";
 		tempString += messageDateFormat.toString() + ":  ";
 		tempString += messageText;
+		
+		if(messageAttachments != null && messageAttachments.size() > 0){
+			for(int i = 0; i < messageAttachments.size(); i++){
+				tempString += messageAttachments.get(i).toString();
+			}
+		}
+		
 		return tempString;
 	}
 }
